76cbf702a02d1c26a68dc351c336a781d71cf367
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-08 09:31:18 +0400

    Added AuthState Persistence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AuthService } from './core/services/auth.service';
 import { LandingComponent } from './features/home/components/landing/landing.component';
+import { HomeComponent } from './features/home/components/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingComponent],
+  imports: [LandingComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ELMSWeb';
+
+  public authService = inject(AuthService);
+
+  ngOnInit(){
+    this.authService.loadAuthState();
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, Signal, signal, computed } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { LoginRequest } from '../interfaces/auth/login';
 
@@ -10,6 +10,9 @@ import { LoginRequest } from '../interfaces/auth/login';
 export class AuthService {
   private apiUrl = environment.baseUrl;
   private http = inject(HttpClient);
+
+  private _isAuthenticated = signal(false);
+  readonly isAuthenticated: Signal<boolean> = computed(() => this._isAuthenticated());
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -21,9 +24,26 @@ export class AuthService {
     const body = login;
 
     return this.http.post(url, body, this.httpOptions).pipe(
+      tap(() => {
+        this._isAuthenticated.set(true);
+        this.saveAuthState();
+      }),
       catchError(() => {
         return throwError(() => new Error("Login Error. Please try again later"))
       })
     );
+  }
+
+  saveAuthState() {
+    localStorage.setItem('isAuthenticated', JSON.stringify(this._isAuthenticated()));
+  }
+
+  loadAuthState() {
+    const isAuthenticated = JSON.parse(localStorage.getItem('isAuthenticated') || 'false');
+    this._isAuthenticated.set(isAuthenticated);
+  }
+
+  clearAuthState() {
+    localStorage.removeItem('isAuthenticated');
   }
 }
