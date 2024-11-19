65c5df55c5963ff71b9fa69bf6aeb230dcb19d29
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-19 15:32:21 +0400

    Added AddEmployee method to ListEmployees Component

--- src/app/features/employees/components/list-employees/list-employees.component.ts
+++ src/app/features/employees/components/list-employees/list-employees.component.ts
@@ -2,6 +2,9 @@ import { Component, inject, signal } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
 import { EmployeeResponse } from '../../../../core/interfaces/employee/IEmployee';
 import { ListEmployeesModules } from '../../modules/list-employees.module';
+import { CreateEmployeeComponent } from '../create-employee/create-employee.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-employees',
@@ -16,14 +19,34 @@ export class ListEmployeesComponent {
   employees = signal<EmployeeResponse[]>([]);
 
   private employeeService = inject(EmployeeService);
+  private dialog = inject(MatDialog);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.getEmployees();
   }
 
-  getEmployees(): void {
-    this.employeeService.getEmployees().subscribe(result => {
-      this.employees.set(result);
-    })
+  private getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (result) => this.employees.set(result),
+      error: () => this.toastr.error('Failed to load employees', 'Error')
+    });
   }
+
+  addEmployee(): void {
+    const dialogRef = this.dialog.open(CreateEmployeeComponent, {
+      height: 'auto',
+      width: '600px',
+    })
+
+    dialogRef.afterClosed().subscribe({
+      next: (result: EmployeeResponse) => {
+        if(result){
+          this.employees.update(currentEmployees => [...currentEmployees, result]);
+          this.toastr.success(`Employee: ${result.firstName} ${result.lastName} successfully added`, 'Success');
+        }
+      }
+    });
+  }
+
 }
