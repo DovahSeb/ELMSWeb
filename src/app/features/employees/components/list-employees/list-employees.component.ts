a69f6199f5bee4c2c5b99fb90362fb48d08b8b2e
Sebastien Larue <sebastienlar619@gmail.com>
2024-12-12 14:54:26 +0400

    Modifed getEmployees and AddEmployee in their components

--- src/app/features/employees/components/list-employees/list-employees.component.ts
+++ src/app/features/employees/components/list-employees/list-employees.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
-import { EmployeeResponse } from './../../interfaces/IEmployee';
+import { EmployeeRequest, EmployeeResponse } from './../../interfaces/IEmployee';
 import { ListEmployeesModules } from '../../modules/list-employees.module';
 import { CreateEmployeeComponent } from '../create-employee/create-employee.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -11,42 +11,39 @@ import { ToastrService } from 'ngx-toastr';
   standalone: true,
   imports: [ListEmployeesModules],
   templateUrl: './list-employees.component.html',
-  styleUrl: './list-employees.component.scss'
+  styleUrls: ['./list-employees.component.scss']
 })
 
 export class ListEmployeesComponent {
-
-  employees = signal<EmployeeResponse[]>([]);
-
   private employeeService = inject(EmployeeService);
   private dialog = inject(MatDialog);
   private toastr = inject(ToastrService);
 
-  ngOnInit(): void {
-    this.getEmployees();
-  }
+  employees: Signal<EmployeeResponse[]>;
 
-  private getEmployees(): void {
-    this.employeeService.getEmployees().subscribe({
-      next: (result) => this.employees.set(result),
-      error: () => this.toastr.error('Failed to load employees', 'Error')
-    });
+  constructor(){
+    this.employees = this.employeeService.employees;
+    this.employeeService.getEmployees();
   }
 
   addEmployee(): void {
     const dialogRef = this.dialog.open(CreateEmployeeComponent, {
       height: 'auto',
       width: '600px',
-    })
+    });
 
-    dialogRef.afterClosed().subscribe({
-      next: (result: EmployeeResponse) => {
-        if(result){
-          this.employees.update(currentEmployees => [...currentEmployees, result]);
-          this.toastr.success(`Employee: ${result.firstName} ${result.lastName} successfully added`, 'Success');
-        }
+    dialogRef.afterClosed().subscribe((result: EmployeeRequest | null) => {
+      if (result) {
+        this.handleEmployeeCreation(result);
       }
     });
   }
 
+  private handleEmployeeCreation(newEmployee: EmployeeRequest): void {
+    this.employeeService.createEmployee(newEmployee);
+    this.toastr.success(
+      `Employee: ${newEmployee.firstName} ${newEmployee.lastName} successfully added`,
+      'Success'
+    );
+  }
 }
