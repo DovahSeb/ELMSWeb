8db1dd9638b46d7840f97f445de8e9ef7fbe677d
Sebastien Larue <sebastienlar619@gmail.com>
2024-12-23 09:37:48 +0400

    Modified Create Employee Component

--- src/app/features/employees/components/create-employee/create-employee.component.ts
+++ src/app/features/employees/components/create-employee/create-employee.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, inject, Signal, effect, signal, computed, ChangeDetectionStrategy } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { NgForm } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { EmployeeRequest } from './../../interfaces/IEmployee';
 import { ReferenceValueService } from '../../../../core/services/reference-value.service';
@@ -14,59 +13,63 @@ import { ToastrService } from 'ngx-toastr';
   imports: [CreateEmployeesModule],
   templateUrl: './create-employee.component.html',
   styleUrl: './create-employee.component.scss',
-  providers: [DatePipe]
+  providers: [DatePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class CreateEmployeeComponent {
-
   private referenceService = inject(ReferenceValueService);
   private dialogRef = inject(MatDialogRef<CreateEmployeeComponent>);
   private toastr = inject(ToastrService);
   private datePipe = inject(DatePipe);
 
-  employeeForm: EmployeeRequest = this.initializeEmployeeForm();
-  departments: Signal<DepartmentResponse[]>;
+  employeeForm = signal<EmployeeRequest>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    dateAdded: '',
+    departmentId: -1,
+  });
+
   readonly maxDate = new Date();
 
-  constructor(){
+  departments: Signal<DepartmentResponse[]>;
+  isFormValid = computed(() => {
+    const form = this.employeeForm();
+    return (
+      form.firstName.trim() !== '' &&
+      form.lastName.trim() !== '' &&
+      form.email.trim() !== '' &&
+      form.departmentId > 0 &&
+      form.dateAdded !== ''
+    );
+  });
+
+  constructor() {
     this.departments = this.referenceService.departments;
-    this.referenceService.getDepartments();
+    effect(() => {
+      this.referenceService.getDepartments();
+    });
   }
 
-  saveNewEmployee(form: NgForm) {
-    if(!form.valid){
+  saveNewEmployee() {
+    if (!this.isFormValid()) {
       this.showValidationError();
       return;
     }
-    this.formatDateAdded();
-    this.createEmployee();
+
+    this.employeeForm.update((form) => ({
+      ...form,
+      dateAdded: this.datePipe.transform(form.dateAdded, 'yyyy-MM-dd') ?? '',
+    }));
+
+    this.dialogRef.close(this.employeeForm());
   }
 
   closeDialog(): void {
     this.dialogRef.close();
   }
 
-  private initializeEmployeeForm(): EmployeeRequest {
-    return {
-      firstName: '',
-      lastName: '',
-      email: '',
-      dateAdded: '',
-      departmentId: -1,
-    };
-  }
-
   private showValidationError(): void {
     this.toastr.error('Please fill in the required fields', 'Validation Error');
-  }
-
-  private formatDateAdded(): void {
-    if (this.employeeForm.dateAdded) {
-      this.employeeForm.dateAdded = this.datePipe.transform(this.employeeForm.dateAdded, 'yyyy-MM-dd') ?? '';
-    }
-  }
-
-  private createEmployee(): void {
-    this.dialogRef.close(this.employeeForm);
   }
 }
